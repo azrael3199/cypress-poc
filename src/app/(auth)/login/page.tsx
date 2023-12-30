601bbb4556cd411bf9af024872e0f7b7f094c8dd
azrael3199 <chinmayghungurde@gmail.com>
2023-12-30 13:51:36 +0530

    fix: Type errors in auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -99,7 +99,7 @@ const LoginPage = () => {
           {!isLoading ? "Login" : <Loader />}
         </Button>
         <span className="self-container">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-primary">
             Sign Up
           </Link>
